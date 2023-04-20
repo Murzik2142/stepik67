a53875f6386a47d7593e1621417b7415561d5603
Murzabaev Marat <m.murzabaev@rt-solar.ru>
2023-04-20 07:40:51 +0300

    add commit for 1.9

--- 1.9 Логические операции/1.9-8.py
+++ 1.9 Логические операции/1.9-8.py
@@ -1,3 +1,12 @@
+'''
+Найдите результат выражения для заданных значений a и b.
+Учитывайте регистр символов при ответе.
+
+a = True
+b = False
+a and b or not a and not b
+'''
+
 a = True
 b = False
 if ((a and b) or ((not a) and (not b))):
