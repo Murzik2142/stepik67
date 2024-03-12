30cfb6ef6df3f7b53f927efb5a3dca51b425646d
Murzabaev Marat <m.murzabaev@rt-solar.ru>
2024-03-12 08:10:03 +0300

    add generate ip addr in hello world

--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,5 @@
 print("Hello world")
+for i in range(1, 2094):
+    # print("Второй октет: ", i // 254, " Третий октет: ", i % 256)
+    ip_addr="11." + str(i // 254) +"."+str(i % 256) + ".1/24"
+    print(ip_addr)
